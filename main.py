d1cf70e92e5985bd329920904edb1e8634b7914e
larongg <shestikpetr@gmail.com>
2022-10-16 20:10:44 +0700

    Configs xd trash

--- main.py
+++ main.py
@@ -2,4 +2,28 @@ import logging
 from configs import token
 from aiogram import Bot, Dispatcher, executor, types
 
+logging.basicConfig(level=logging.INFO)
+
 bot = Bot(token)
+dp = Dispatcher(bot)
+
+
+otvet = {
+    "А" : [],
+    "Б" : [],
+    "В" : [],
+    }
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.reply("Расставляй в порядке приоритета")
+
+
+@dp.message_handler()
+async def xd(message: types.Message):
+    pass
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
